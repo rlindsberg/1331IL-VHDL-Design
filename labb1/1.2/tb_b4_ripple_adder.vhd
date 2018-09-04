93ae89b21ada24041a349c4feb5a7ca1117c01bf
rlindsberg <roderick@karlemstrand.com>
2018-09-04 11:45:12 +0200

    4 Bit Ripple Adder sim passed

--- labb1/1.2/tb_b4_ripple_adder.vhd
+++ labb1/1.2/tb_b4_ripple_adder.vhd
@@ -22,37 +22,37 @@ Signal num1: integer;
 Signal num2: integer;
 Signal A_int: std_logic_vector (3 downto 0);
 Signal B_int : std_logic_vector (3 downto 0);
-Signal adder_cin : std_logic;
+Signal Cin : std_logic;
 
 --outputs
-Signal answer : std_logic_vector (3 downto 0);
-Signal overflow : std_logic;
+Signal Sum : std_logic_vector (3 downto 0);
+Signal Overflow : std_logic;
 
 Begin
 
   DUT: b4_ripple_adder Port Map(
 		A => A_int,
 		B => B_int,
-		Cin =>adder_cin,
-		Cout => overflow,
-		Sum => answer
+		Cin => Cin,
+		Cout => Overflow,
+		Sum => Sum
 	);
 
 	process
 	Begin
 		num1 <= 8;
 		num2 <= 1;
-		adder_cin <='0';
-		wait for 10 ns;
+		Cin <= '0';
+		wait for 50 ns;
 
 		num1 <= 2;
 		num2 <= 2;
-		adder_cin <='0';
-		wait for 10 ns;
+		Cin <= '0';
+		wait for 50 ns;
 
 	End process;
 
-	A_int <= std_logic_vector(to_unsigned(num1,a_int'length));
-	B_int <= std_logic_vector(to_unsigned(num2,b_int'length));
+	A_int <= std_logic_vector(to_unsigned(num1, A_int'length));
+	B_int <= std_logic_vector(to_unsigned(num2, A_int'length));
 
 End architecture;
