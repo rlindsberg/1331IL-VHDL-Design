99e3306ccb603112588f58e257587e1355c1b3b7
gitgnmn <amanda@gnmn.se>
2018-09-06 12:47:47 +0200

    folders and entity part of files for 2.3 and 2.4

---
+++ labb1/2.3 Adder/Adder.vhd
@@ -0,0 +1,6 @@
+entity Adder is
+  port(
+    a : unsigned(7 downto 0);
+    b : unsigned(7 downto 0)
+
+  )
---
+++ labb1/2.4 adder/adder.vhd
@@ -0,0 +1,7 @@
+entity adder is
+  port (
+    a   : std_logica_vector(7 downto 0);
+    b   : std_logica_vector(7 downto 0);
+    sum : out std_logica_vector(7 downto 0)
+  );
+end entity;
