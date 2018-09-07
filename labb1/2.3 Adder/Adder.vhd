08676adfc774ba8faae1c337fad24aa7dee89c9a
rlindsberg <roderick@karlemstrand.com>
2018-09-07 12:46:07 +0200

    Design 8 bit adder without carry

--- labb1/2.3 Adder/Adder.vhd
+++ labb1/2.3 Adder/Adder.vhd
@@ -1,6 +1,18 @@
+Library IEEE;
+Use IEEE.STD_LOGIC_ARITH.ALL;
+Use WORK.ALL;
+
 entity Adder is
   port(
     a : unsigned(7 downto 0);
-    b : unsigned(7 downto 0)
+    b : unsigned(7 downto 0);
+    sum: out unsigned(7 downto 0)
+  );
+End;
 
-  )
+Architecture behavioural of Adder is
+  Begin
+
+    sum <= a + b After 5 ns;
+
+End behavioural;
