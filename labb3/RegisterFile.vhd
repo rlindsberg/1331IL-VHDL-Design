9e20f996e08bd88ff713ce0d94cface7a5504e7b
rlindsberg <roderick@karlemstrand.com>
2018-09-15 23:36:09 +0200

    Add register file structure, begin with process

--- labb3/RegisterFile.vhd
+++ labb3/RegisterFile.vhd
@@ -1,16 +1,25 @@
 Entity RegisterFile is
-    Port(   clk: in std_logic;
-            data_in: IN std_logic;
-            data_out_1: IN std_logic;
-            data_out_0: OUT data_word;
-            sel_in: OUT data_word;
-            sel_out_1: IN std_logic_vector (1 downto 0);
-            sel_out_0: IN std_logic_vector (1 downto 0);
-            rw_reg: in std_logic
-    );
+  Port( clk: in std_logic;
+        data_in: IN std_logic;
+        data_out_1: IN std_logic;
+        data_out_0: OUT data_word;
+        sel_in: OUT data_word;
+        sel_out_1: IN std_logic_vector (1 downto 0);
+        sel_out_0: IN std_logic_vector (1 downto 0);
+        rw_reg: in std_logic
+  );
 End;
 
 Architecture behavioural of RegisterFile is
-    Begin
+  Begin
+    //declare register file
+    Type registerFile is array(0 to 3) of std_logic_vector(data_word downto 0);
+    Signal register: registerFile;
 
+    Process(clk)
+      Begin
+        if rising_edge(clk) then
+
+        end if;
+    End Process;
 End behavioural;
