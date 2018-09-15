abb47accd68c5965e6ab4f7a6e282649dbcf7751
rlindsberg <roderick@karlemstrand.com>
2018-09-15 23:40:50 +0200

    Fix typo bugs

--- labb3/RegisterFile.vhd
+++ labb3/RegisterFile.vhd
@@ -1,18 +1,18 @@
 Entity RegisterFile is
   Port( clk: in std_logic;
-        data_in: IN std_logic;
-        data_out_1: IN std_logic;
-        data_out_0: OUT data_word;
-        sel_in: OUT data_word;
-        sel_out_1: IN std_logic_vector (1 downto 0);
-        sel_out_0: IN std_logic_vector (1 downto 0);
+        data_in: in std_logic;
+        data_out_1: out data_word;
+        data_out_0: out data_word;
+        sel_in: in std_logic_vector (1 downto 0);
+        sel_out_1: in std_logic_vector (1 downto 0);
+        sel_out_0: in std_logic_vector (1 downto 0);
         rw_reg: in std_logic
   );
 End;
 
 Architecture behavioural of RegisterFile is
   Begin
-    //declare register file
+    -- declare register file
     Type registerFile is array(0 to 3) of std_logic_vector(data_word downto 0);
     Signal register: registerFile;
 
