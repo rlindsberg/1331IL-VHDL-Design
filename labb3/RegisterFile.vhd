c5bc63f468b50ebf1b8473b36998bb20476022a1
rlindsberg <roderick@karlemstrand.com>
2018-09-15 23:55:03 +0200

    Done register file architecture

--- labb3/RegisterFile.vhd
+++ labb3/RegisterFile.vhd
@@ -19,7 +19,19 @@ Architecture behavioural of RegisterFile is
     Process(clk)
       Begin
         if rising_edge(clk) then
-
+          -- read before write to register file
+          data_out_0 <= register(to_integer(unsigned(sel_out_0)));
+          data_out_1 <= register(to_integer(unsigned(sel_out_1)));
+          -- write upon re_reg is low
+          if rw_reg = '0' then
+            -- sel_in chooses to which register data_in is written
+            register(to_integer(unsigned(sel_in))) <= data_in;
+          end if;
+          -- bypass the new written values to output
+          if falling_edge then
+            data_out_0 <= register(to_integer(unsigned(sel_out_0)));
+            data_out_1 <= register(to_integer(unsigned(sel_out_1)));
+          end if;
         end if;
     End Process;
 End behavioural;
