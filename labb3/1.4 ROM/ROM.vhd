19785e062964a8a17da3e8a2f496632361b9b8e1
rlindsberg <roderick@karlemstrand.com>
2018-09-16 19:07:48 +0200

    Skeleton for ROM

---
+++ labb3/1.4 ROM/ROM.vhd
@@ -0,0 +1,15 @@
+Entity ROM is
+  Port(
+    addr: in address_bus;
+    data: out instruction_bus;
+    ce: in std_logic
+  );
+End;
+
+Architecture behavioural of ROM is
+  Signal
+  Begin
+    Process
+      Begin
+
+End behavioural;
