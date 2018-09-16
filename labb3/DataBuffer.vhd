45b5885d3ad074c3e69df0f8bbf7b1f0bd6592c5
rlindsberg <roderick@karlemstrand.com>
2018-09-16 07:24:49 +0200

    Skeleton for data buffer

---
+++ labb3/DataBuffer.vhd
@@ -0,0 +1,15 @@
+Entity DataBuffer is
+  Port(
+    out_en: in std_logic;
+    data_in: in data_word;
+    data_out: out data_bus
+  );
+End;
+
+Architecture behavioural of DataBuffer is
+
+  Signal
+
+Begin
+
+End behavioural;
