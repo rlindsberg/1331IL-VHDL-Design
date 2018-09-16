ec463dc6d8c830c559bceedaa3d2a3f54261508c
rlindsberg <roderick@karlemstrand.com>
2018-09-16 11:54:24 +0200

    Done data buffer

--- labb3/DataBuffer.vhd
+++ labb3/DataBuffer.vhd
@@ -12,4 +12,16 @@ Architecture behavioural of DataBuffer is
 
 Begin
 
+  Process(clk)
+    Begin
+      if rising_edge(clk) then
+        -- pass data_in to data_out when out_en is high
+        if out_en = '1' then
+          data_out <= data_in;
+        else
+          -- data_out remains high impedans when out_en is low
+          data_out <= 'Z';
+        end if;
+      end if;
+
 End behavioural;
