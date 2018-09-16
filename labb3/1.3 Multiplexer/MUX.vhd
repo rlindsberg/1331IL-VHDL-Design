084286a3f1e150883b463f2dcecec0805b654903
rlindsberg <roderick@karlemstrand.com>
2018-09-16 13:35:28 +0200

    Done multiplexer

--- labb3/1.3 Multiplexer/MUX.vhd
+++ labb3/1.3 Multiplexer/MUX.vhd
@@ -11,5 +11,15 @@ End;
 Architecture behavioural of MUX is
   Signal
   Begin
+    -- 00 selects data_in_0
+    if sel = "00" then
+      data_out <= data_in_0;
+    -- 01 selects data_in_1
+    elseif sel = "01" then
+      data_out <= data_word(data_in_1);
+    -- 10 selects data_in_2
+    elseif sel = "10" then
+      data_out <= data_in_2;
+    end if;
 
 End behavioural;
