f110f7aa16de91534d278d0bb54eb4ad8cfe0982
rlindsberg <roderick@karlemstrand.com>
2018-09-22 08:03:54 +0200

    Reset counter and add out_clk

--- labb4/1.1 16 Counter/Counter.vhd
+++ labb4/1.1 16 Counter/Counter.vhd
@@ -12,6 +12,25 @@ Entity Counter is
     in_clk      : in std_logic;
     in_step     : in std_logic;
     in_reset    : in std_logic;
-    out_counter : out std_logic
+    out_clk     : out std_logic;
+    out_counter : out std_logic_vector(data_size-1 downto 0)
   );
 End;
+
+Architecture arch of Counter is
+
+Begin
+
+  Process(in_clk, in_step)
+    Variable internal_counter: std_logic_vector(data_size-1 downto 0);
+    Begin
+      if in_reset = '1' then
+        out_clk <= '0';
+        out_counter <= (others=>'0'); -- reset to 0000
+      end if;
+
+      if clk'event AND clk='1' then
+
+      end if;
+
+End Architecture;
