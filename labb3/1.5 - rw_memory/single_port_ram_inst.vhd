7692c0b17d678e9e2f973b9276e3c9ac72d7ebf1
rlindsberg <roderick@karlemstrand.com>
2018-09-23 18:16:29 +0200

    Remove conflict files

--- labb3/1.5 - rw_memory/single_port_ram_inst.vhd
+++
@@ -1,8 +0,0 @@
-single_port_ram_inst : single_port_ram PORT MAP (
-		address	 => address_sig,
-		clock	 => clock_sig,
-		data	 => data_sig,
-		rden	 => rden_sig,
-		wren	 => wren_sig,
-		q	 => q_sig
-	);
