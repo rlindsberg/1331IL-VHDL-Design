05529421c8d2b0c72df2d75aa1858c7e094ebb87
gitgnmn <amanda@gnmn.se>
2018-09-24 15:30:33 +0200

    1.5 compiles, simulation weird...

--- labb3/1.5 - rw_memory/rw_memory.vhd
+++ labb3/1.5 - rw_memory/rw_memory.vhd
@@ -12,34 +12,22 @@ Entity rw_memory is
 End Entity;
 
 Architecture behaviour of rw_memory is
-  Component single_port_ram
-    Port(   address		: IN STD_LOGIC_VECTOR (11 DOWNTO 0);
-        		clock     : IN STD_LOGIC  := '1';
-        		data      : IN STD_LOGIC_VECTOR (7 DOWNTO 0);   --data in
-        		rden      : IN STD_LOGIC  := '1';               --enable
-        		wren      : IN STD_LOGIC;                       --read-write
-        		q         : OUT STD_LOGIC_VECTOR (7 DOWNTO 0)   --data out
-	  );
-  End Component;
-
-  Signal data_in, data_out  : data_bus;
-
+  Type data_array is array (0 to 15) of data_bus;
+  Signal internal_data : data_bus;
 Begin
-  R : single_port_ram port map(adr, clk, data_in, ce, rw, data_out); -- Maybe not then. Remake single_port_ram into rw_memory?
-
+  
   Process(clk)
+    Variable data_table : data_array;
   Begin
-    if rising_edge(clk) then
-      if ce = '1' then
-        data <= (others => 'Z');
+    if rising_edge(clk) and ce = '0' then
+      if rw = '0' then
+        data_table(to_integer(unsigned(adr))) := data;
 
-      elsif (ce = '0' and rw = '0') and clk = '1' then
-        data_in <= data;
-
-      elsif ce = '0' and rw = '1' then
-        data <= data_out;
-
-      end if;
+      elsif rw = '1' then
+        internal_data <= data_table(to_integer(unsigned(adr)));
+		  
+		end if;
     End if;
   End Process;
+  data <= (others => 'Z') when ce = '1' else internal_data;
 End Architecture;
--- labb3/1.5 - rw_memory/tb_rw_memory.vhd
+++ labb3/1.5 - rw_memory/tb_rw_memory.vhd
@@ -19,95 +19,98 @@ Architecture test of tb_rw_memory is
   Signal data           :   data_bus;
   Signal clk, ce, rw    :   std_logic;
 
-  Signal out_data         :   data_bus;
-  Signal test_data      :   data_bus;
-
 Begin
-  test_data <= ( test_data'length-1 downto 5 =>'0',
-  4 downto 0 =>'1');
-
   RM : rw_memory port map(adr, data, clk, ce, rw);
 
   Process
+    Variable test_data  :  data_bus;
+    Variable z_data     :  data_bus;
   Begin
+    test_data := ( test_data'length-1 downto 2 =>'0', --0011
+                   1 downto 0 =>'1');
+    z_data := (others => 'Z');
 
     -- write to mem when ce = '0' rw = '0'
     -- read from mem when ce = '0' rw = '0'
+    -- should write to mem.
     ce <= '0';
     rw <= '0';
     wait for 2 ns;
 
+    adr <= "0000";
     data <= test_data;
 
     wait for 2 ns;
 
     rw <= '1';
-    out_data <= data;
 
-    assert out_data = test_data
+    assert data = test_data
     report "Values does not match ce=0 rw=0"
     severity warning;
 
-    -- "zero" memory
-    data <= (others => 'U');
-    wait for 2 ns;
+    rw <= '0';
+    wait for 1 ns;
 
     -- write to mem when ce = '0' rw = '1'
     -- read from mem when ce = '0' rw = '1'
+    -- should read from mem.
     ce <= '0';
     rw <= '1';
     wait for 2 ns;
 
+    adr <= "0001";
     data <= test_data;
 
     wait for 2 ns;
 
     rw <= '1';
-    out_data <= data;
 
-    assert out_data /= test_data
+    assert data /= test_data
     report "Values does not match ce=0 rw=1"
     severity warning;
 
-    -- "zero" memory
-    data <= (others => 'U');
-    wait for 2 ns;
+    wait for 1 ns;
 
     -- write to mem when ce = '1' rw = '0'
     -- read from mem when ce = '1' rw = '0'
+    -- should become ZZZZ...
     ce <= '1';
     rw <= '0';
     wait for 2 ns;
 
+    adr <= "0010";
     data <= test_data;
 
     wait for 2 ns;
 
     rw <= '1';
-    out_data <= data;
+    data <= data;
 
-    assert out_data = (others => 'Z')
+    assert data = z_data
     report "Values does not match ce=1 rw=0"
     severity warning;
 
-    -- "zero" memory
-    data <= (others => 'U');
-    wait for 2 ns;
+    wait for 1 ns;
+
     -- write to mem when ce = '1' rw = '1'
     -- read from mem when ce = '1' rw = '1'
+    -- Should become ZZZZ...
     ce <= '1';
     rw <= '1';
     wait for 2 ns;
 
+    adr <= "0011";
     data <= test_data;
 
     wait for 2 ns;
 
     rw <= '1';
-    out_data <= data;
 
-    assert out_data = (others => 'Z')
+    assert data = z_data
     report "Values does not match ce=1 rw=1"
     severity warning;
+
+    wait for 1 ns;
+
   End Process;
 End Architecture;
