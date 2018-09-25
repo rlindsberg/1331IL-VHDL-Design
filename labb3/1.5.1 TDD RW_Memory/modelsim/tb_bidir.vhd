d6f63b697d9c3df8dc224efe1afe7d14b5c8b0dd
rlindsberg <roderick@karlemstrand.com>
2018-09-25 11:48:39 +0200

    Add ce and rw

--- labb3/1.5.1 TDD RW_Memory/modelsim/tb_bidir.vhd
+++ labb3/1.5.1 TDD RW_Memory/modelsim/tb_bidir.vhd
@@ -6,32 +6,30 @@ Entity tb_bidir is End Entity;
 architecture arch of tb_bidir is
   component bidir
   port (
-    Z  : inout std_logic_vector(3 downto 0);
-    Y  : out std_logic_vector(3 downto 0);
-    OE : in  std_logic;
-    WE : in    std_logic;
-    A  : in  std_logic_vector(3 downto 0)
+    Z  : inout  std_logic_vector(3 downto 0);
+    ce : in     std_logic;
+    rw : in     std_logic;
+    A  : in     std_logic_vector(3 downto 0)
   );
   end component bidir;
 
 
   signal Z_inout: std_logic_vector(3 downto 0);
-  signal Y_out: std_logic_vector(3 downto 0);
-  signal OE_in: std_logic;
-  signal WE_in: std_logic;
+  signal ce_in: std_logic;
+  signal rw_in: std_logic;
   signal A_in: std_logic_vector(3 downto 0);
 
   begin
-    bidir_ins: bidir port map(Z_inout, Y_out, OE_in, WE_in, A_in);
+    bidir_ins: bidir port map(Z_inout, ce_in, rw_in, A_in);
 
     sim: process
     Begin
     A_in <= "1010";
-    OE_in <= '0';
-    WE_in <= '1';
+    ce_in <= '0';
+    rw_in <= '1';
     wait for 5 ns;
 
-    OE_in<= '1';
+    ce_in<= '1';
     wait for 5 ns;
 
   end process;
