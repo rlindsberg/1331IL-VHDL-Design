a032d9278188a3ebce6decbf7522aa51874696a1
rlindsberg <roderick@karlemstrand.com>
2018-09-25 14:24:28 +0200

    Add clk and mem with many errors

--- labb3/1.5.1 TDD RW_Memory/modelsim/bidir.vhd
+++ labb3/1.5.1 TDD RW_Memory/modelsim/bidir.vhd
@@ -1,8 +1,11 @@
-library ieee;
-use ieee.std_logic_1164.all;
+Library IEEE;
+Use IEEE.std_logic_1164.all;
+Use IEEE.numeric_std.all;
+Use work.cpu_package.all;
 
 entity bidir is
     port (
+        clk:       std_logic;
         Z  : inout  std_logic_vector(3 downto 0);
         ce : in     std_logic;
         rw : in     std_logic;
@@ -11,10 +14,26 @@ entity bidir is
 end Entity bidir;
 
 architecture behave of bidir is
+
+  Type data_array is array (0 to 15) of data_bus;
+  Signal mem: data_array;
+
 begin
 
-    Z <= A        when (ce = '0' and rw = '1') else -- read
-         "ZZZZ"   when (ce = '1')              else -- disabled
-         "XXXX";                                    -- unknown
+  -- Memory Write Block
+  -- Write Operation : When we = 1, cs = 1
+    MEM_WRITE: process (clk) begin
+      if rising_edge(clk) and ce = '0' and rw = '0' then
+        mem(0) <= A;
+      end if;
+    end process;
 
+    -- Memory Read Block
+     MEM_READ: process (clk) begin
+       if (rising_edge(clk) and ce = '0' and rw = '1') then
+         Z <= mem(0);
+       else
+         Z <= (others=>'0');
+       end if;
+     end process;
 end behave;
--- labb3/1.5.1 TDD RW_Memory/modelsim/tb_bidir.vhd
+++ labb3/1.5.1 TDD RW_Memory/modelsim/tb_bidir.vhd
@@ -6,6 +6,7 @@ Entity tb_bidir is End Entity;
 architecture arch of tb_bidir is
   component bidir
   port (
+    clk:       std_logic;
     Z  : inout  std_logic_vector(3 downto 0);
     ce : in     std_logic;
     rw : in     std_logic;
@@ -13,20 +14,20 @@ architecture arch of tb_bidir is
   );
   end component bidir;
 
-
+  Signal clk_in: std_logic;
   signal Z_inout: std_logic_vector(3 downto 0);
   signal ce_in: std_logic;
   signal rw_in: std_logic;
   signal A_in: std_logic_vector(3 downto 0);
 
   begin
-    bidir_ins: bidir port map(Z_inout, ce_in, rw_in, A_in);
+    bidir_ins: bidir port map(clk_in, Z_inout, ce_in, rw_in, A_in);
 
     sim: process
     Begin
     A_in <= "1010";
     ce_in <= '0';
-    rw_in <= '1';
+    rw_in <= '0';
     wait for 5 ns;
 
     ce_in<= '1';
