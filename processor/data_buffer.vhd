8fffb1bd3fbb617a544f76d042dad9c48c342e76
gitgnmn <amanda@gnmn.se>
2018-10-05 19:27:35 +0200

    renamed ports of mux and buffer

--- processor/data_buffer.vhd
+++ processor/data_buffer.vhd
@@ -3,19 +3,19 @@ Use IEEE.std_logic_1164.all;
 use work.cpu_package.all;
 
 Entity data_buffer is
-  Port(   out_en    :     std_logic;
-          data_in   :     data_word;
-          data_out  : out data_word);
+  Port(   in_out_en    :     std_logic;
+          in_data_in   :     data_word;
+          out_data_out  : out data_word);
 End Entity;
 
 architecture rtl of data_buffer is
 begin
-  process(out_en, data_in)
+  process(in_out_en, in_data_in)
   begin
-    if out_en = '1' then
-      data_out <= data_in;
+    if in_out_en = '1' then
+      out_data_out <= in_data_in;
     else
-      data_out <= (others => 'Z');
+      out_data_out <= (others => 'Z');
     end if;
   end process;
 end architecture;
--- processor/multiplexer.vhd
+++ processor/multiplexer.vhd
@@ -4,18 +4,18 @@ Use IEEE.numeric_std.all;
 use work.cpu_package.all;
 
 Entity multiplexer is
-  Port(   sel         :     std_logic_vector(1 downto 0);
-          data_in_2   :     data_word;
-          data_in_1   :     data_word;
-          data_in_0   :     data_word;
-          data_out    : out data_word);
+  Port(   in_sel         :     std_logic_vector(1 downto 0);
+          in_data_in_2   :     data_word;
+          in_data_in_1   :     data_word;
+          in_data_in_0   :     data_word;
+          out_data_out    : out data_word);
 End Entity;
 
 Architecture rtl of multiplexer is
 Begin
-  with sel select
-    data_out <= data_in_0 when "00",
-	              data_in_1 when "01",
-				        data_in_2 when "10",
-				        (others=>'U') when others;
+  with in_sel select
+    out_data_out <= in_data_in_0 when "00",
+	                  in_data_in_1 when "01",
+				            in_data_in_2 when "10",
+				            (others=>'U') when others;
 End Architecture;
